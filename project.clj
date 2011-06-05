d9bf2462b0d655b7c08fc029303423d6411046c6
Paul Ingles <paul@oobaloo.co.uk>
2011-06-06 01:28:20 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject zeromq-sample "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.mikejs/clojure-zmq "2.0.7-SNAPSHOT"]
+                 [org.zeromq/jzmq "2.1.0-SNAPSHOT"]]
+  :native-path "/usr/local/lib")
---
+++ test/zeromq_sample/test/core.clj
@@ -0,0 +1,6 @@
+(ns zeromq-sample.test.core
+  (:use [zeromq-sample.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
